00a56426576d89c932549a321458a7771d51604d
L1STOV <listovspartak99@gmail.com>
2020-08-11 16:16:58 +0300

    sum square difference

---
+++ sum_square_difference.py
@@ -0,0 +1,12 @@
+sum_of_squares = 0
+sum_ = 0
+
+for x in range(0, 101):
+    sum_of_squares += x ** 2
+    sum_ += x
+
+result = sum_of_squares - sum_
+
+print(f"Sum of squares = {sum_of_squares}")
+print(f"Squared sum = {sum_}")
+print(f"Result of minus = {result}")
